f1815abdb319e7e1ccdeddaa3290b839dccdaeb8
dorrkainfo <info@dorrka.com>
2026-06-10 01:40:49 +0530

    config: add APP_MASTER_KEY to ecosystem.backend-only.cjs

--- ecosystem.backend-only.cjs
+++ ecosystem.backend-only.cjs
@@ -11,6 +11,7 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 9000,
+        APP_MASTER_KEY: "05009b0aadc9bb8af82aaca45b6614b815277967b4cf20cac133b24da63225a5",
       },
     },
     {
